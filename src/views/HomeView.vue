fb5692b290d0f6be0e3b9809bd366d6d3289b9ac
Herman <smurovnemez@mail.ru>
2022-10-28 15:40:09 -0500

    init: vue setup

---
+++ src/views/HomeView.vue
@@ -0,0 +1,57 @@
+
+<template>
+  <div class="home">
+ 
+    <form @submit.prevent="sendData">
+      <div class="form-control">
+        <label for="name">Name</label>
+         <input v-model="data.name" id="name" type="text">
+       </div>
+       <div class="form-control">
+         <label for="email">Email</label>
+         <input v-model="data.email" id="email" type="email">
+       </div>
+       <div class="form-control">
+         <label for="address">Address</label>
+         <input v-model="data.address" id="address" type="text">
+       </div>
+       <div class="form-control">
+         <label for="gender">Gender</label>
+        <span>Male <input v-model="data.gender" id="gender" type="radio" value="male"></span>
+         <span>Female <input v-model="data.gender" id="gender" type="radio" value="female"></span>
+       </div>
+     <input type="submit" class="send" value="Send">
+    </form>
+  </div>
+ </template>
+
+ <script setup>
+import { reactive } from "@vue/reactivity";
+import { computed } from "@vue/runtime-core";
+
+ const data = reactive({
+       name: '',
+       email: '',
+       address: '',
+       gender: '',
+      })
+  
+      function sendData() {
+        console.log(data);
+      }
+
+    
+
+  </script>
+
+  <style>
+  .form-control {
+     padding: 5px;
+    } 
+    .form-control label {
+       display: block;
+    }
+    .send {
+       margin: 5px
+      }
+  </style>
---
+++ src/views/Thanks.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <div class="thanks">
+            <h1>Thank you for your record!</h1>
+        </div>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
